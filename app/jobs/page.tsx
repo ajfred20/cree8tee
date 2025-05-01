ac86a64450b01003922729019465f5c183b33667
Fred Aj <138402744+ajfred20@users.noreply.github.com>
2025-05-01 10:03:16 +0100

    Update page.tsx

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -294,7 +294,15 @@ const JobsPage = () => {
         
         {/* Background decoration */}
         <div className="absolute top-1/2 right-0 transform -translate-y-1/2 w-64 h-64 bg-purple-100 rounded-full opacity-20 -mr-32"></div>
-        <div className="absolute bottom-0 left-1/4 w-48 h-48 bg-purple-100 rounded-full opacity-10"></div>
+        <div className="absolute bottom-0 left-1/4 w-48 h-48 bg-purple-100 rounded-full opacity-10">
+                <Image
+                  src="/assets/web3-preview.jpg"
+                  alt="Web3 professional working"
+                  width={1000}
+                  height={500}
+                  className="w-full h-auto object-cover rounded-lg"
+                  priority
+                /></div>
       </section>
     </div>
   );
